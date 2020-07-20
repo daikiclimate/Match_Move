db4a2c4534c73086bc5c1bcec3edcf81aea8a225
Your Name <daikiclimate041@gmail.com>
2020-07-20 11:12:01 +0900

    match move func

--- match_move.py
+++ match_move.py
@@ -1,20 +1,28 @@
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
-import utils
 import time
 import pprint
+import argparse
 
-def get_images(magazine, box, ad):
-    mag_img = cv2.imread(magazine)
-    mag_img = cv2.cvtColor(mag_img, cv2.COLOR_BGR2RGB)
+def load_args():
+    parser = argparse.ArgumentParser(description='match move')  
+    parser.add_argument('base', help='置き換えるよう画像。探索する大きい画像')    
+    parser.add_argument('box', help='探索したい領域')
+    parser.add_argument('ad', help='探索してきた領域を置き換える用画像')
+    parser.add_argument('--output',default = "output/output.jpg" ,help='探索してきた領域を置き換える用画像')
+    parser.add_argument('--param',default = 1 ,help='探索してきた領域を置き換える用画像')
+    args = parser.parse_args()
+    return args
+
+
+def get_images(src, box, ad):
+
+    src_img = cv2.imread(src)
 
     box_img = cv2.imread(box)
-    box_img = cv2.cvtColor(box_img, cv2.COLOR_BGR2RGB)
 
     ad_img = cv2.imread(ad)
-    ad_img = cv2.cvtColor(ad_img, cv2.COLOR_BGR2RGB)
-    return mag_img, box_img, ad_img
+    return src_img, box_img, ad_img
 
 def get_match_features(src, target):
     akaze = cv2.AKAZE_create()
@@ -32,20 +40,15 @@ def get_match_features(src, target):
             [kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
     dst_pts = np.float32(
            [kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
-    src = cv2.cvtColor(src, cv2.COLOR_RGB2BGR)
-    target = cv2.cvtColor(target, cv2.COLOR_RGB2BGR)
     cv2.imwrite('draw_match.jpg', cv2.drawMatches(src, kp1, target, kp2, matches[:10], None, flags=2))
     return src_pts, dst_pts
 
 def get_homography(src_pts, dst_pts):
-    # h = utils.calculate_homography_matrix(src_pts.reshape(-1,2), dst_pts.reshape(-1,2))
     h, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC)
-
-    # h = utils.calculate_homography_matrix(dst_pts.reshape(-1,2), src_pts.reshape(-1,2))
     return h
 
-def transform_homography(ad_img, h, im2):
-    ad_img = cv2.warpPerspective(ad_img, h, (im2.shape[1],im2.shape[0]))
+def transform_homography(ad_img, h, after_img_shape):
+    ad_img = cv2.warpPerspective(ad_img, h, (after_img_shape.shape[1],after_img_shape.shape[0]))
     return ad_img
 
 def fusion(src, generated):
@@ -61,10 +64,8 @@ def video_function(base , box = "images/box1.jpg", ad = "images/ad2.jpg"):
     mag_img = base[:]
 
     box_img = cv2.imread(box)
-    box_img = cv2.cvtColor(box_img, cv2.COLOR_BGR2RGB)
 
     ad_img = cv2.imread(ad)
-    ad_img = cv2.cvtColor(ad_img, cv2.COLOR_BGR2RGB)
 
     p = 20
     orgHeight, orgWidth = box_img.shape[:2]
@@ -92,44 +93,51 @@ def video_function(base , box = "images/box1.jpg", ad = "images/ad2.jpg"):
     output = fusion(mag_img, transformed_ad)
     return output,psnr
 
-def main(base = "images/mag1.jpg", box = "images/box1.jpg", ad = "images/ad2.jpg", output_name = "output.jpg"):
-
+# def main(base = "images/mag1.jpg", box = "images/box1.jpg", ad = "images/ad2.jpg", output_name = "output.jpg"):
+def main():
+    args = load_args()
+    base = args.base
+    box = args.box
+    ad = args.ad
+    output_name = args.output
+    param = int(args.param)
 
     print("start")
     print(output_name)
     t1 = time.time()
-    mag_img, box_img, ad_img = get_images(base, box, ad)
-    ad_img[ad_img==0] = 1
+    base_img, box_img, ad_img = get_images(base, box, ad)
 
-    # box_img = cv2.resize(box_img, (ad_img.shape[1],ad_img.shape[0]))
+
+    #うまくいかない際のパラメータ
+    #box画像があまりにも大きいとおかしくなる時がある
+    #その時は小さくする
     orgHeight, orgWidth = box_img.shape[:2]
-    size = (orgWidth//5,orgHeight//5)
+    size = (orgWidth//param,orgHeight//param)
     box_img = cv2.resize(box_img, size)
 
+    #差し替え用広告画像の初期設定。必須
+    ad_img[ad_img==0] = 1
     ad_img = cv2.resize(ad_img, (box_img.shape[1], box_img.shape[0]))
-    # m = cv2.rectangle(mag_img, (1,270), (1690,945), (0, 0, 0),thickness=-1)
 
+    #特徴点抽出
+    src_pts, dst_pts = get_match_features(base_img ,box_img)
 
-    # src_pts, dst_pts = get_match_features(box_img ,m)
-    src_pts, dst_pts = get_match_features(box_img ,mag_img)
-    # print(src_pts.shape)
-
-    h = get_homography(src_pts, dst_pts)
-    # h = get_homography(dst_pts, src_pts)
+    #ホモグラフィー行列の導出抽出
+    h = get_homography(dst_pts, src_pts)
     pprint.pprint(h)
 
-    transformed_ad = transform_homography(ad_img, h, mag_img)
-    # cv2.imwrite("tmp.jpg", transformed_ad)
+    #広告画像の変換
+    transformed_ad = transform_homography(ad_img, h, base_img)
 
-    output = fusion(mag_img, transformed_ad)
-    output = cv2.cvtColor(output, cv2.COLOR_RGB2BGR)
-    cv2.imwrite(output_name, output)
+    #特定領域のみを置き換え
+    output_img = fusion(base_img, transformed_ad)
+
+    #書き込み
+    cv2.imwrite(output_name, output_img)
     print("finish")
-    print("time:",round(time.time()-t1),"sec")
-    return output
-
+    print("time:",round(time.time()-t1,2),"sec")
 
 
 if __name__=="__main__":
-    # main()
-    main("images/fail.png", "images/box5.png", "images/ad3.png","puma.jpg")
+    main()
+    # main("images/fail.png", "images/box5.png", "images/ad3.png","puma.jpg")
